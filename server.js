37cde746d84bbaa83eac82042cb30cea40505e0c
X0riax <lucas.tear@efrei.net>
2024-04-02 18:47:46 +0200

    ajout client

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const { port, mongoURI } = require('./config');
-const bcrypt = require('bcryptjs');
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -102,76 +101,6 @@ app.delete('/remove-from-cart/:id', (req, res) => {
 app.get('/cart', (req, res) => {
     try {
         res.json(Object.values(cart));
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Server Error');
-    }
-});
-
-const userSchema = new mongoose.Schema({
-    nom: String,
-    prenom: String,
-    mail: String,
-    mot_de_passe: String,
-});
-
-const User = mongoose.model('Users', userSchema);
-
-app.post('/add-user', async (req, res) => {
-    try {
-        const { nom, prenom, email, password } = req.body;
-        const existingUser = await User.findOne({ email });
-        if (existingUser) {
-            return res.status(400).send('Cet email est déjà utilisé');
-        }
-        const hashedPassword = password;//await bcrypt.hash(password, 10);
-        const user = new User({ nom, prenom, mail: email, mot_de_passe: hashedPassword });
-        await user.save();
-
-        res.status(201).send('User added successfully');
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Server Error');
-    }
-});
-
-app.get('/users', async (req, res) => {
-    try {
-        const users = await User.find();
-        res.json(users);
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Server Error');
-    }
-});
-
-app.post('/login', async (req, res) => {
-    const { mail, mot_de_passe } = req.body;
-    try {
-        const user = await User.findOne({ mail });
-        if (!user) {
-            return res.status(401).send('Mail ou Mot de Passe incorrects');
-        }
-        const isMatch = await bcrypt.compare(mot_de_passe, user.mot_de_passe);
-        if (isMatch) {
-            res.status(200).json({ nom: user.nom, prenom: user.prenom });
-        } else {
-            res.status(401).send('Mail ou Mot de Passe incorrects');
-        }
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Erreur du serveur');
-    }
-});
-
-app.delete('/delete-user/:id', async (req, res) => {
-    try {
-        const user = await User.findById(req.params.id);
-        if (!user) {
-            return res.status(404).send('User not found');
-        }
-        await User.deleteOne({ _id: req.params.id });
-        res.status(200).send('User deleted successfully');
     } catch (err) {
         console.error(err);
         res.status(500).send('Server Error');
