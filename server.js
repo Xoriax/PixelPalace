39e0d345b6d4ad8d5b21af935773e4a5cb421fc4
X0riax <lucas.tear@efrei.net>
2024-04-14 11:08:05 +0200

    ajout SignUp

--- server.js
+++ server.js
@@ -5,18 +5,11 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const { port, mongoURI } = require('./config');
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
+const bcrypt = require('bcrypt');
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
-app.use(session({
-    secret: 'votre_secret',
-    resave: false,
-    saveUninitialized: true,
-    store: MongoStore.create({ mongoUrl: mongoURI })
-}));
 
 mongoose.connect(mongoURI)
     .then(() => console.log('MongoDB database is connected'))
@@ -127,6 +120,37 @@ app.get('/pderive/license/:license', async (req, res) => {
     } catch (err) {
         console.error(err);
         res.status(500).send('Server Error');
+    }
+});
+
+const userSchema = new mongoose.Schema({
+    nom: String,
+    prenom: String,
+    email: String,
+    password: String
+});
+
+const User = mongoose.model('users', userSchema);
+
+app.post('/signup', async (req, res) => {
+    const { nom, prenom, email, password } = req.body;
+
+    try {
+        const existingUser = await User.findOne({ email });
+
+        if (existingUser) {
+            return res.status(400).json({ message: 'Cet email est déjà utilisé.' });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = new User({ nom, prenom, email, password: hashedPassword });
+        await newUser.save();
+
+        res.status(201).json({ message: 'Inscription réussie !' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Erreur serveur lors de l\'inscription.' });
     }
 });
 
